c6b693837632eafdc32ae5ac99d1abdb9c450097
zalec <142818531+z4le3c@users.noreply.github>
2023-10-24 07:08:49 -0600

    fix design to grid layout

--- src/mainPage.js
+++ src/mainPage.js
@@ -14,24 +14,27 @@ const loadPage = () => {
 
     loadSection(
         `The interior of "Elysian Eats" exudes sophistication, with its soft, ambient lighting casting a warm glow on plush velvet upholstery and polished marble tables. Crystal chandeliers glisten overhead, casting dancing shadows on the walls adorned with original works of art. The air is filled with a symphony of aromas, a tantalizing preview of the culinary delights awaiting you.`,
-        'https://images.pexels.com/photos/7441761/pexels-photo-7441761.jpeg',
+        'https://images.pexels.com/photos/4253127/pexels-photo-4253127.jpeg',
     );
     loadSection(
         `Our world-renowned chefs, artisans of flavor, have curated a menu that reflects their passion for culinary excellence. Each dish is a masterpiece, a harmonious blend of exquisite ingredients sourced from around the globe, artfully plated to tantalize the senses. Whether you crave succulent seafood, tender cuts of meat, or inspired vegetarian creations, "Elysian Eats" offers a feast for every palate.`,
         'https://images.pexels.com/photos/3338497/pexels-photo-3338497.jpeg?auto=compress&cs=tinysrgb&w=1600',
     );
+    loadSection(
+        ``,
+        'https://images.pexels.com/photos/4253320/pexels-photo-4253320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    );
 }
 
 const loadSection = (text, imgURL) => {
-    let sectionDiv = document.createElement('div');
-    sectionDiv.classList.add('section')
-    sectionDiv.style.backgroundImage = `URL(${imgURL})`;
+    let imgTag = document.createElement('img');
+    imgTag.setAttribute('src',imgURL);
     let textDiv = document.createElement('div');
     textDiv.classList.add('text');
     textDiv.textContent = text
-    sectionDiv.appendChild(textDiv);
 
-    contentDiv.appendChild(sectionDiv);
+    contentDiv.appendChild(imgTag);
+    contentDiv.appendChild(textDiv);
 }
 
 export {loadPage}
